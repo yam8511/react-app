87123aaab3fd4089f83072c894488c22084dd3f7
Zuolar <yam8511@gmail.com>
2017-11-10 01:38:45 +0800

    todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '@/assets/logo.svg';
-import './App.css';
+import '@/assets/css/App.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
+import './assets/css/index.css';
+import App from './components/App';
 import HelloMessage from './components/HelloMessage';
+import TodoApp from './components/TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <div>
         <App />
-        <HelloMessage name="Zuolar" />
+        <HelloMessage />
+        <TodoApp />
     </div>
     , document.getElementById('root'));
 registerServiceWorker();
